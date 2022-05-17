909edac6edbc0fbffd182466422efbf250e16bf5
njfix@msn.com <njfix@msn.com>
2022-05-17 22:37:44 +0800

    build: vite别名@配置和使用

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,10 @@ export default defineConfig({
     eslintPlugin({
       // 配置选项
     })
-  ]
+  ],
+  resolve: {
+    alias: {
+      '@': path.join(__dirname, 'src')
+    }
+  }
 })
