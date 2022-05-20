c77ed86749e4b9f23097423f923fc81dfeb77ae2
傅新 <njfix@msn.com>
2022-05-20 09:44:33 +0800

    build: element-plus 配置

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
 import path from 'path'
+import {
+  createStyleImportPlugin,
+  ElementPlusResolve
+} from 'vite-plugin-style-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +13,21 @@ export default defineConfig({
     vue(),
     eslintPlugin({
       // 配置选项
+    }),
+    createStyleImportPlugin({
+      resolves: [
+        ElementPlusResolve()
+      ],
+      libs: [
+        // If you don’t have the resolve you need, you can write it directly in the lib, or you can provide us with PR
+        {
+          libraryName: 'ant-design-vue',
+          esModule: true,
+          resolveStyle: (name) => {
+            return `ant-design-vue/es/${name}/style/index`
+          }
+        }
+      ]
     })
   ],
   resolve: {
